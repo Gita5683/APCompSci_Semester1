e9891129691d9cc59583678cd86eb382a96e2d9d
Gitanjali Multani <gita.pm1@gmail.com>
2016-09-02 13:34:31 -0700

    Added Files

---
+++ APLesson_01/Lab01_asciiart.java
@@ -0,0 +1,18 @@
+public class Lab01_asciiart
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  \"\"   \"\"  ");
+		System.out.println("   @ . . @  ");
+		System.out.println("   (-------)  ");					 
+		System.out.println("  (  >_<  )  ");					
+		System.out.println("^^  ~~   ^^  ");
+
+		System.out.println("      _  ");
+		System.out.println("  _( \" )_ ");
+		System.out.println(" (_  .  _)");
+		System.out.println("   /  :  \\ ");
+		System.out.println(" (_/   \\_)");
+		
+	}
+}
---
+++ APLesson_01/Lab01_asciiart_2.java
@@ -0,0 +1,12 @@
+public class Lab01_asciiart_2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("      _  ");
+		System.out.println("  _( \" )_ ");
+		System.out.println(" (_  .  _)");
+		System.out.println("   /  :  \\ ");
+		System.out.println(" (_/   \\_)");
+		
+	}
+}
