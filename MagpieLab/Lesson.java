a21852dafccf7fb3aca04017c933e989f40384c5
Gita5683 <gita.pm1@gmail.com>
2017-03-27 13:06:57 -0700

    Completed MagPie Lab

---
+++ MagpieLab/Lesson.java
@@ -0,0 +1,15 @@
+public class Lesson
+{
+	public static void main(String [] args)
+	{
+		String a = "ASDFASF";
+		System.out.println(a.toLowerCase());
+		
+		String name = "zazzcccccccczazzzzzccccc";
+		System.out.println(name.indexOf("zazz", 5));
+		
+		String b = "    Yo!    ";
+		System.out.println("\"" + b + "\"");
+		System.out.println("\"" + b.trim() + "\"");
+	}
+}
